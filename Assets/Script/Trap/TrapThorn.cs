8b7d8b865c17293547f32c4d6bef64e3f4bf4ea0
N0I0C0K <nick131410@aliyun.com>
2022-07-23 09:55:49 +0800

    修改了伤害方块计算

--- Assets/Script/Trap/TrapThorn.cs
+++ Assets/Script/Trap/TrapThorn.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Player;
 using UnityEngine;
 
 namespace Trap
@@ -11,6 +12,8 @@ namespace Trap
     {
         [Range(1, 10)]
         public int damage = 1;            //伤害数值（在外面手动输入）
+        public float maxStayTime = 3f;
+        private float stayTime = 0f;
         private void Awake()
         {
             if (damage == 0)
@@ -20,7 +23,17 @@ namespace Trap
         }
         public override void onPlayerEnter(Player.PlayerAttribute playerAttribute)
         {
+            stayTime = 0f;
             playerAttribute.playerHealth.CauseDamage(damage);      //对玩家造成伤害
+        }
+        public override void onPlayerStay(PlayerAttribute playerAttribute)
+        {
+            stayTime += Time.deltaTime;
+            if (stayTime >= maxStayTime)
+            {
+                playerAttribute.playerHealth.CauseDamage(damage);
+                stayTime = 0;
+            }
         }
     }
 
